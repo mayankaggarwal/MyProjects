854068f9f892b6aa98dc085533dc49ca8dfd9d0f
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2017-05-24 09:57:54 +0530

    Extracting 1 user

--- InstacartMarketBasketAnalysis/TestScript.R
+++ InstacartMarketBasketAnalysis/TestScript.R
@@ -15,8 +15,10 @@ else{
 aisle <- read.csv(file.path(dataFolder,'aisles.csv'),header=TRUE,sep=',',stringsAsFactors = FALSE);
 departments <- read.csv(file.path(dataFolder,'departments.csv'),header=TRUE,sep=',',stringsAsFactors = FALSE);
 order_products_prior <- read.csv(file.path(dataFolder,'order_products__prior.csv'),header=TRUE,sep=',',stringsAsFactors = FALSE);
-order_products__train <- read.csv(file.path(dataFolder,'order_products__train.csv'),header=TRUE,sep=',',stringsAsFactors = FALSE);
+order_products_train <- read.csv(file.path(dataFolder,'order_products__train.csv'),header=TRUE,sep=',',stringsAsFactors = FALSE);
 orders <- read.csv(file.path(dataFolder,'orders.csv'),header=TRUE,sep=',',stringsAsFactors = FALSE);
 products <- read.csv(file.path(dataFolder,'products.csv'),header=TRUE,sep=',',stringsAsFactors = FALSE);
 sample_submission <- read.csv(file.path(dataFolder,'sample_submission.csv'),header=TRUE,sep=',',stringsAsFactors = FALSE);
 
+order_user1 <- orders[orders$user_id==1,c('order_id')]
+order_products_prior[order_products_prior$order_id %in% order_user1,]
